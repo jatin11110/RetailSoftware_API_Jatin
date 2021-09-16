29731ea0619f42a935f9f0fde97080e0f8b20545
Jatinkumar11110@gmail.com <Jatinkumar11110@gmail.com>
2021-09-16 12:44:18 +0300

    Add project files.

---
+++ RetailSoftware_API_Jatin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RetailSoftware_API_Jatin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
